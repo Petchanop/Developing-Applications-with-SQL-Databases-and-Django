b964e00225b96ca41a3c544acb780e686632c066
Petchanop <nop.hide@gmail.com>
2021-09-07 09:13:08 +0000

    Change in admin

--- final_template/onlinecourse/admin.py
+++ final_template/onlinecourse/admin.py
@@ -21,6 +21,11 @@ class CourseAdmin(admin.ModelAdmin):
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [QuestionInline]
+
+class ChoiceAdmin(admin.ModelAdmin):
+    inlines = [Choiceinline]
 
 # <HINT> Register Question and Choice models here
 
